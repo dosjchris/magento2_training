bfac73b4f42a920485566a625884cd6415bca8a2
christian.dosjoub@gmx.fr <christian.dosjoub@gmx.fr>
2017-11-30 11:23:26 +0100

    seller 2

---
+++ app/code/Training/Seller/Controller/Router.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: training
+ * Date: 11/30/17
+ * Time: 10:45 AM
+ */
+
+namespace Training\Seller\Controller;
+
+
+
+use Magento\Framework\App\ActionFactory;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\Action\Forward as ForwardAction;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\RouterInterface;
+
+class Router implements RouterInterface
+{
+    /**
+     * @var ActionFactory
+     */
+    protected $actionFactory;
+
+    /**
+     * PHP Constructor
+     *
+     * @param ActionFactory $actionFactory
+     */
+    public function __construct(
+        ActionFactory $actionFactory
+    ) {
+        $this->actionFactory = $actionFactory;
+    }
+
+    /**
+     * Match application action by request
+     *
+     * @param RequestInterface $request
+     * @return ActionInterface
+     */
+    public function match(RequestInterface $request)
+    {
+        /** @var \Magento\Framework\App\Request\Http $request */
+        $url = $request->getPathInfo();
+
+        if ($url === '/sellers.html') {
+            $request->setPathInfo('/seller/seller/index');
+            return $this->actionFactory->create(ForwardAction::class);
+        }
+
+        if (preg_match('%^/seller/(.+)\.html$%', $url, $match)) {
+            $request->setPathInfo(sprintf('/seller/seller/view/identifier/%s', $match[1]));
+            return $this->actionFactory->create(ForwardAction::class);
+        }
+
+
+    }
+}
